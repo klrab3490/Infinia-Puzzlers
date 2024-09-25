b5abe84b0dc36af91e5f3f5ecbfa72e4bdf006f4
Rahul Lap <rahulbushi69@gmail.com>
2024-09-25 13:01:04 +0530

    feat: Add @radix-ui/react-label and @radix-ui/react-tabs dependencies
    This commit adds the following dependencies to the project:
    - @radix-ui/react-label@2.1.0
    - @radix-ui/react-tabs@1.1.0
    
    These dependencies are required for upcoming UI components and functionality.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 import { ThemeProvider } from "@/components/theme-provider"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/login";
+import RegisterPage from "./pages/register";
+
 
 function App() {
-  return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <div className="flex justify-center items-center min-h-screen">
-        
-      </div>
-    </ThemeProvider>
-  )
+    return (
+        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+            {/* <AuthProvider> */}
+                <Router>
+                    <Routes>
+
+                        <Route path="/" element={<LoginPage />} />
+                        <Route path="/register" element={<RegisterPage />} />
+                    </Routes>
+                </Router>
+            {/* </AuthProvider> */}
+        </ThemeProvider>
+    )
 }
 
-export default App
+export default App;
