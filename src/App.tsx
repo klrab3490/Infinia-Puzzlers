ec479c73cf7bd4f389f6374c230272481629f730
Rahul Lap <rahulbushi69@gmail.com>
2024-09-25 23:17:08 +0530

    refactor: Complete al the registration and other parts.
    - Whats left out is to check if the tasks are being fetched properly and upload of image is also correct

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,357 @@
-import { ThemeProvider } from "@/components/theme-provider"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LoginPage from "./pages/login";
-import RegisterPage from "./pages/register";
+import { motion } from "framer-motion"; // Import motion for animations
+import { ThemeProvider } from "@/components/theme-provider";
+
+// Import From ShadCN
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+
+// Firebase imports
+import { FormEvent, useState } from "react";
+import { collection, doc, getDoc, getDocs, serverTimestamp, setDoc } from "firebase/firestore";
+import { auth, db } from "@/config/firebaseConfig";
+import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
+import Navbar from "./components/navbar";
+
+interface TeamMembers {
+    name: string;
+    email: string;
+}
+
+interface Task {
+    id: string;
+    taskName: string;
+    description: string;
+    isCompleted: boolean;
+    accepted: boolean;
+    grade: number;
+    photoURL: string | null;
+}
+
+interface TeamData {
+    teamName: string;
+    teamMembers: TeamMembers[];
+    tasks: Task[];
+}
 
 
 function App() {
+    const [user, setUser] = useState(false);
+    const [userID, setUserID] = useState("");
+    const [teamData, setTeamData] = useState<TeamData>({
+        teamName: "",
+        teamMembers: [
+            { name: "", email: "" },
+            { name: "", email: "" },
+            { name: "", email: "" },
+            { name: "", email: "" }
+        ],
+        tasks: []
+    });
+    const [error, setError] = useState("");
+    const [message, setMessage] = useState("");
+    const [tasks, setTasks] = useState<Task[]>([]);
+    const [teamDataSubmitted, setTeamDataSubmitted] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+
+    const handleTeamDataSubmit = async (e: FormEvent) => {
+        e.preventDefault();
+        setIsLoading(true);
+        try {
+            console.log("Team data submitted:", teamData);
+            await setDoc(doc(db, "teams", userID), {
+                ...teamData,
+                userID: userID,
+                registeredAt: serverTimestamp()
+            });
+            setTeamDataSubmitted(true);
+            // Save team data to Firestore or handle it as needed
+            await fetchTasks(); // Fetch tasks after submitting team data
+        } catch (error) {
+            setError("Error submitting team data:");
+            if (error instanceof Error) {
+                setMessage(error.message);
+            } else {
+                setMessage("An unknown error occurred");
+            }
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+    const fetchTasks = async () => {
+        try {
+            const tasksCollection = collection(db, "tasks");
+            const taskSnapshot = await getDocs(tasksCollection);
+            const tasksData = taskSnapshot.docs.map(doc => {
+                const data = doc.data();
+                return {
+                    id: doc.id,
+                    taskName: data.taskName || "",
+                    description: data.description || "",
+                    isCompleted: data.isCompleted || false,
+                    accepted: data.accepted || false,
+                    grade: data.grade || 0,
+                    photoURL: data.photoURL || null
+                };
+            });
+            setTasks(tasksData);
+        } catch (error) {
+            setError("Error submitting team data:");
+            if (error instanceof Error) {
+                setMessage(error.message);
+            } else {
+                setMessage("An unknown error occurred");
+            }
+        }
+    };
+
+    const handleInputChange = (index: number, field: keyof TeamMembers, value: string) => {
+        const updatedMembers = [...teamData.teamMembers];
+        updatedMembers[index] = { ...updatedMembers[index], [field]: value };
+        setTeamData({ ...teamData, teamMembers: updatedMembers });
+    };
+
+    // Login
+    const handleLogin = async (e: FormEvent) => {
+        e.preventDefault();
+        setIsLoading(true);
+        const target = e.target as typeof e.target & {
+            loginEmail: { value: string };
+            loginPassword: { value: string };
+        };
+
+        const email = target.loginEmail.value;
+        const password = target.loginPassword.value;
+
+        try {
+            // Handle login
+            const userCredential = await signInWithEmailAndPassword(auth, email, password);
+            const loggedInUser = userCredential.user.uid;
+            setUserID(loggedInUser);
+            setUser(true);  // Assuming `user` indicates login status
+            console.log("Logged In User ID:", loggedInUser);
+            setMessage("Login successful!");
+            checkData(loggedInUser);
+        } catch (error) {
+            setError("Error logging in:");
+            if (error instanceof Error) {
+                setMessage(error.message);
+            } else {
+                setMessage("An unknown error occurred");
+            }
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+    // Register
+    const handleRegister = async (e: FormEvent) => {
+        e.preventDefault();
+        setIsLoading(true);
+        const target = e.target as typeof e.target & {
+            registerEmail: { value: string };
+            registerPassword: { value: string };
+            confirmPassword: { value: string };
+        };
+    
+        const email = target.registerEmail.value;
+        const password = target.registerPassword.value;
+        const confirmPassword = target.confirmPassword.value;
+    
+        // Check if passwords match
+        if (password !== confirmPassword) {
+            setError("Passwords do not match.");
+            setIsLoading(false);
+            return;
+        }
+    
+        try {
+            // Firebase create user logic
+            const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+            const registeredUser = userCredential.user.uid;
+    
+            // Update the user state or redirect
+            setUserID(registeredUser);
+            setUser(true);  // Assuming `user` indicates login status
+            setMessage("Registration successful!");
+    
+            console.log("Registered User ID:", registeredUser);
+        } catch (error) {
+            if (error instanceof Error) {
+                setError("Error registering user");
+                setMessage(error.message);
+            } else {
+                setMessage("An unknown error occurred");
+            }
+        } finally {
+            setIsLoading(false);
+        }
+    };
+    
+    const handleSignOut = async () => {
+        setUserID("");
+        setUser(false);
+        setTeamDataSubmitted(false);
+        setTeamData({
+            teamName: "",
+            teamMembers: [
+                { name: "", email: "" },
+                { name: "", email: "" },
+                { name: "", email: "" },
+                { name: "", email: "" }
+            ],
+            tasks: []
+        });
+        setTasks([]);
+        setError("");
+        setMessage("");
+        setIsLoading(false);
+    }
+
+    const checkData = async (id: string) => {
+        const docRef = doc(db, "teams", id);
+        const docSnap = await getDoc(docRef);
+        if (docSnap.exists()) {
+            console.log("Document data:", docSnap.data());
+            setTeamDataSubmitted(true);
+        } else {
+            console.log("No such document!");
+        }
+    }
+
     return (
         <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-            {/* <AuthProvider> */}
-                <Router>
-                    <Routes>
+            {userID && ( <Navbar onSignOut={handleSignOut} /> )}
+            <div className="p-6 flex flex-col justify-center items-center min-h-screen w-full bg-gray-900 text-white">
+                {!user ? (
+                    <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+                        <Card className="w-[350px]">
+                            <CardHeader className="space-x-1">
+                                <CardTitle className="text-2xl">Welcome Back</CardTitle>
+                                <CardDescription>Enter your email to sign in or create an account</CardDescription>
+                            </CardHeader>
+                            <Tabs defaultValue="login" className="w-full">
+                                <div className="px-6 pb-6">
+                                    <TabsList className="grid w-full grid-cols-2">
+                                        <TabsTrigger value="login">Login</TabsTrigger>
+                                        <TabsTrigger value="register">Register</TabsTrigger>
+                                    </TabsList>
+                                </div>
 
-                        <Route path="/" element={<LoginPage />} />
-                        <Route path="/register" element={<RegisterPage />} />
-                    </Routes>
-                </Router>
-            {/* </AuthProvider> */}
+                                {/* Login Form with animation */}
+                                <TabsContent value="login">
+                                    <motion.div
+                                        initial={{ opacity: 0, scale: 0.95 }}
+                                        animate={{ opacity: 1, scale: 1 }}
+                                        transition={{ duration: 0.5 }}
+                                    >
+                                        <form onSubmit={handleLogin}>
+                                            <CardContent className="space-y-4">
+                                                <div className="space-y-2">
+                                                    <Label htmlFor="loginEmail">Email</Label>
+                                                    <Input type="email" id="loginEmail" placeholder="john@gmail.com" required />
+                                                </div>
+                                                <div className="space-y-2">
+                                                    <Label htmlFor="loginPassword">Password</Label>
+                                                    <Input type="password" id="loginPassword" placeholder="********" required />
+                                                </div>
+                                            </CardContent>
+                                            <CardFooter>
+                                                <Button className="w-full" type="submit" disabled={isLoading}>
+                                                    {isLoading ? "Signing In..." : "Sign In"}
+                                                </Button>
+                                            </CardFooter>
+                                        </form>
+                                    </motion.div>
+                                </TabsContent>
+
+                                {/* Registration Form with animation */}
+                                <TabsContent value="register">
+                                    <motion.div
+                                        initial={{ opacity: 0, scale: 0.95 }}
+                                        animate={{ opacity: 1, scale: 1 }}
+                                        transition={{ duration: 0.5 }}
+                                    >
+                                        <form onSubmit={handleRegister}>
+                                            <CardContent className="space-y-4">
+                                                <div className="space-y-2">
+                                                    <Label htmlFor="registerEmail">Email</Label>
+                                                    <Input type="email" id="registerEmail" placeholder="john@gmail.com" required />
+                                                </div>
+                                                <div className="space-y-2">
+                                                    <Label htmlFor="registerPassword">Password</Label>
+                                                    <Input type="password" id="registerPassword" placeholder="********" required />
+                                                </div>
+                                                <div className="space-y-2">
+                                                    <Label htmlFor="confirmPassword">Confirm Password</Label>
+                                                    <Input type="password" id="confirmPassword" placeholder="********" required />
+                                                </div>
+                                            </CardContent>
+                                            <CardFooter>
+                                                <Button className="w-full" type="submit" disabled={isLoading}>
+                                                    {isLoading ? "Registering..." : "Register"}
+                                                </Button>
+                                            </CardFooter>
+                                        </form>
+                                    </motion.div>
+                                </TabsContent>
+                            </Tabs>
+                        </Card>
+                    </motion.div>
+                ) : (
+                    <>
+                        {!teamDataSubmitted ? (
+                            <motion.form onSubmit={handleTeamDataSubmit} className="border dark:border-gray-700 p-5 rounded-3xl gap-6 shadow-lg bg-gray-800" initial={{ scale: 0.9, opacity: 0 }} animate={{ scale: 1, opacity: 1 }} transition={{ duration: 0.5 }}>
+                                <h2 className="text-center text-4xl mb-6">Enter Team Details</h2>
+                                <div className="mb-4">
+                                    <label className="block text-xl mb-1">Team Name:</label>
+                                    <Input type="text" value={teamData.teamName} onChange={(e) => setTeamData({ ...teamData, teamName: e.target.value })} className="w-full border-b border-gray-400 focus:border-blue-500 bg-transparent" required />
+                                </div>
+                                {teamData.teamMembers.map((member, index) => (
+                                    <div key={index} className="flex flex-col mb-4">
+                                        <h3 className="text-lg font-semibold">Member {index + 1}</h3>
+                                        <div className="flex gap-4 ml-5">
+                                            <div>
+                                                <label className="block text-md mb-1">Name:</label>
+                                                <Input type="text" value={member.name} onChange={(e) => handleInputChange(index, "name", e.target.value)} className="w-full border-b border-gray-400 focus:border-blue-500 bg-transparent" required />
+                                            </div>
+                                            <div>
+                                                <label className="block text-md mb-1">Email:</label>
+                                                <Input type="email" value={member.email} onChange={(e) => handleInputChange(index, "email", e.target.value)} className="w-full border-b border-gray-400 focus:border-blue-500 bg-transparent" required />
+                                            </div>
+                                        </div>
+                                    </div>
+                                ))}
+                                <Button type="submit" className="w-full mt-4" disabled={isLoading}>
+                                    {isLoading ? "Submitting..." : "Submit Team Data"}
+                                </Button>
+                            </motion.form>
+                        ) : (
+                            <motion.div initial={{ y: -50, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 0.7 }}>
+                                <h2 className="text-3xl mb-6">Your Tasks</h2>
+                                {tasks.length > 0 ? (
+                                    tasks.map((task) => (
+                                        <motion.div key={task.id} className="task bg-gray-800 p-4 mb-4 rounded-lg shadow" initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5 }} >
+                                            <h3 className="text-2xl mb-2">{task.taskName}</h3>
+                                            <p className="mb-4">{task.description}</p>
+                                            <Input type="file" accept="image/*" className="w-full border-b border-gray-400 focus:border-blue-500 bg-transparent" />
+                                            <Button className="w-full mt-2">Upload</Button>
+                                        </motion.div>
+                                    ))
+                                ) : (
+                                    <p>No tasks available.</p>
+                                )}
+                                {error && <p className="text-red-500">{message}</p>}
+                            </motion.div>
+                        )}
+                    </>
+                )}
+            </div>
         </ThemeProvider>
-    )
+    );
 }
 
 export default App;
